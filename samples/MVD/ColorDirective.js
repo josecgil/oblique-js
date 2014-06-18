5c27379b557277595efa1b4cd3ec951c412f544e
José Carlos Gil Zambrana <josecgil@gmail.com>
2014-06-19 00:50:52 +0200

    Better new sample

---
+++ samples/MVD/ColorDirective.js
@@ -0,0 +1,24 @@
+var ColorDirective = function (element, colors) {
+    var jqElement=$(element);
+    var self=this;
+    jqElement.click(function(event){
+        event.preventDefault();
+        var colorName=jqElement.html();
+        var sizes=self.findSizes(colorName, colors);
+        var html=Oblique().renderHtml("/oblique-js/samples/MVD/sizes.hbs",sizes);
+        $("#sizes").html(html);
+    });
+};
+
+ColorDirective.prototype.findSizes=function(colorName, colors) {
+    for(var i=0;i<colors.length;i++) {
+        var color=colors[i];
+        if (color.name==colorName) {
+            return color.sizes;
+        }
+    }
+};
+
+ColorDirective.CSS_EXPRESSION = "*[data-colors]";
+
+Oblique().registerDirective(ColorDirective);
